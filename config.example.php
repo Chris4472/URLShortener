43df0dc7760fba02f6bdad6c8ca2196aae6b3190
Chris4472 <chrisdawsondeveloper@gmail.com>
2024-02-28 19:35:12 +0000

    Add Config File
    Added example config file with database credentials

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+#Remove .example from filename and enter password to your localhost
+$servername = "localhost";
+$username   = "root";
+$password   = ""; #Enter password here
+$dbname     = "url_shorterner";
+?>
